2b6e690bb48566df5fe065e2b7733328fdd129a9
juanpabloglobalsys98 <juanpabloperezdevelopment@gmail.com>
2022-10-24 22:57:47 -0500

    Update module1.ts

--- src/app/modulo1-cuestionario/modulo1-cuestionario.component.ts
+++ src/app/modulo1-cuestionario/modulo1-cuestionario.component.ts
@@ -6,8 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./modulo1-cuestionario.component.css']
 })
 export class Modulo1CuestionarioComponent implements OnInit {
-   fragebogen:any;
-   listResp:string[]=['p11','p22','p31','p41','p53','p64'];
+  fragebogen:any;
+  listResp:string[]=['p11','p22','p31','p41','p53','p64'];
+  numAnswer = 0;
 
   constructor() {
     this.fragebogen = {
@@ -24,13 +25,39 @@ export class Modulo1CuestionarioComponent implements OnInit {
   }
 
   onsubmit(){
-    alert("formulario enviado ");
-    console.log(this.fragebogen);
     this.listResp.map( (data) => {
       switch(data){
-
+        case 'p11':{
+          this.fragebogen.pregunt1 === 'p11' ? this.numAnswer += 1 : null;
+          break;
+        }
+        case 'p22':{
+          this.fragebogen.pregunt2 === 'p22' ? this.numAnswer += 1 : null;
+          break;
+        }
+        case 'p31':{
+          this.fragebogen.pregunt3 === 'p31' ? this.numAnswer += 1 : null;
+          break;
+        }
+        case 'p41':{
+          this.fragebogen.pregunt4 === 'p41' ? this.numAnswer += 1 : null;
+          break;
+        }
+        case 'p53':{
+          this.fragebogen.pregunt5 === 'p53' ? this.numAnswer += 1 : null;
+          break;
+        }
+        case 'p64':{
+          this.fragebogen.pregunt6 === 'p64' ? this.numAnswer += 1 : null;
+          break;
+        }
       };
     })
+    if( this.numAnswer == 6){
+      console.log("todo correcto");
+    }else{
+      console.log("incorrecto");
+    }
 
   }
   
