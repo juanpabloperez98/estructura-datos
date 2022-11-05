7f0d597ebefd3e6a3270673c2098176fa9ae9d12
juanpabloglobalsys98 <juanpabloperezdevelopment@gmail.com>
2022-11-05 12:50:32 -0500

    Adding mod1_seccion3 example1

--- src/app/ejemplos-representacion-binaria/ejemplo1/ejemplo1.component.ts
+++ src/app/ejemplos-representacion-binaria/ejemplo1/ejemplo1.component.ts
@@ -11,11 +11,12 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./ejemplo1.component.css']
 })
 export class Ejemplo1Component implements OnInit {
-
-  lines_to_input = [8]
-  lines_to_validate = [9]
+  
+  lines_to_input = [6]
+  lines_to_validate = [7]
+  lines_to_modify_vars = [11,12,13];
   current_line = 1;
-  max_line = 20;
+  max_line = 18;
   run_code = false;
   explain_pass = '';
   value_vars = '';
@@ -29,10 +30,11 @@ export class Ejemplo1Component implements OnInit {
   reload = false;
 
   // Variables del ejemplo
-  password_user = 'MinuevaClave';
-  password = '';
-  equal = true;
+  number = 0;
+  size_array = 0;
+  binary:string[] = []
   i = 0;
+  index = 0;
   inputfield = '';
 
 
@@ -69,80 +71,79 @@ export class Ejemplo1Component implements OnInit {
     },
     {
       'line_explain':'Definimos la variable number de tipo entero',
-      'var_values':{
-        'password_user':'MinuevaClave'
-      },
     },
     {
       'line_explain':'Pedimos al usuario que ingrese un numero',
-      'var_values':{
-        'size':12
-      },
+      'output':'Ingrese un numero',
     },
     {
       'line_explain':'Se guarda el valor ingresado en la variable number',
-      'var_values':{
-        'size':12
-      },
     },
     {
       'line_explain':'Obtenemos la cantidad de bits que se necesitan para representar el numero ingresado por el usuario',
-      'output':'Ingrese contraseña',
-    },
-    {
-      'line_explain':'Se captura lo ingresado por el usuario',
+      'var_values':{
+        'number':""
+      },
     },
     {
       'line_explain':'Definimos un arreglo de caracteres con una longitud de la cantidad de bits obtenidos anteriormente',
       'var_values':{
-        'equal':"true"
+        'size_array':""
       },
     },
     {
       'line_explain':'Definimos el iterador que va a recorrer el arreglo de caracteres',
       'var_values':{
-        'i':this.i,
-        'size':"12"
+        'size_array':""
       },
     },
     {
       'line_explain':'Iteramos hasta que el valor de number sea diferente de cero',
       'var_values':{
-        'i':this.i,
-        'password':'',
-        'password_user':'',
-        'password[i]':'',
-        'password_user[i]':''
+        'number':""
       }
     },
     {
       'line_explain':'validamos con una condición ternaria si el modulo de number entre 2 es igual a cero o no, de ser así entonces se añade al array de caracteres el caracter "0" de lo contario el caracter "1"',
       'var_values':{
-        'equal':'',
+        'number':"",
+        'i':"",
+        'binary[i]':""
       }
     },
     {
       'line_explain':'luego se divide el valor de number entre 2 (de esta manera por cada ciclo se ira disminuyendo el valor)',
-    },
-    {
-      'line_explain':'Se disminuye el valor del iterador',
-    },
-    {
-      'line_explain':'Se finaliza el ciclo anterior',
       'var_values':{
-        'equal':this.equal,
+        'number':"",
       }
     },
     {
+      'line_explain':'Se disminuye el valor del iterador',
+      'var_values':{
+        'i':"",
+      }
+    },
+    {
+      'line_explain':'Se finaliza el ciclo anterior',
+    },
+    {
       'line_explain':'Se define un ciclo for que ira desde 0 hasta el valor de size_array',
-      'output':'Claves iguales',
+      'var_values':{
+        'index':"0",
+        'size_array':"",
+      }
     },
     {
       'line_explain':'Se imprime el caracter en la posición index',
+      'var_values':{
+        'binary[index]':""
+      }
     },
     {
-      'line_explain':'Se imprime que las claves no son iguales',
-      'output':'Claves no iguales',
+      'line_explain':'Se cierra el ciclo for',
+    },
+    {
+      'line_explain':'Fin del programa',
     }
   ]
 
@@ -161,6 +162,24 @@ export class Ejemplo1Component implements OnInit {
   }
 
   // Functions to run program
+  modify_vars = () => {
+    // 11,12,13
+    if ( this.lines_to_modify_vars.includes(this.current_line) ){
+      switch(this.current_line){
+        case 11:
+          var value = this.number % 2 === 0 ? '0' : '1';
+          this.binary.push(value);
+          break;
+        case 12:
+          this.number = Math.floor(this.number / 2);
+          break;
+        case 13:
+          this.i --;
+          break;
+      }
+    }
+  }
+
   refresh = () => {
     window.location.reload();
   }
@@ -178,19 +197,37 @@ export class Ejemplo1Component implements OnInit {
     }
   }
 
-  jump = () => {}
+  jump = () => {
+    switch(this.current_line){
+      case 14:
+        if( this.number != 0 ){
+          this.loop_jump(10,4,2);
+        }        
+        break;
+      case 16:
+        if( this.index >= this.size_array ){
+          this.loop_jump(17,1);
+        }
+        break
+      case 17:
+        if( this.index < this.size_array ){
+          this.loop_jump(15,2,2);
+          this.index += 1;
+        }
+        break
+    }
+  }
 
   validate_input = () => {
-    if ( this.lines_to_validate.includes(this.current_line) ){
-      if ( this.password === '' ){
-        this.toastr.error('Debe ingresar un valor para continuar');
-        this.current_line --;
-        this.less_top();
-      }else if( this.password.length < 12 ){
-        this.toastr.error('La longitud de la cadena debe ser del mismo tamaño que la variable password_user');
+    if ( this.lines_to_validate.includes(this.current_line) ){      
+      if ( this.inputfield === '' || isNaN(parseInt(this.inputfield))){
+        this.toastr.error('Debe ingresar un valor valido para continuar');
         this.current_line --;
         this.less_top();
       }
+      this.number = parseInt(this.inputfield)
+      this.size_array = Math.round(Math.log2(this.number) + 1);
+      this.i = this.size_array - 1;
     }
   }
 
@@ -213,23 +250,25 @@ export class Ejemplo1Component implements OnInit {
       let data = this.code_obj[this.current_line - 1]['var_values'];
       for (const key in data) {
         switch( key ){
-          case 'password':
-            this.value_vars += `<strong>${key}</strong> = ${this.password}<br/>`
+          case 'number':
+            this.value_vars += `<strong>${key}</strong> = ${this.number}<br/>`
             break;
-          case 'password_user':
-            this.value_vars += `<strong>${key}</strong> = ${this.password_user}<br/>`
+          case 'size_array':
+            this.value_vars += `<strong>${key}</strong> = ${this.size_array}<br/>`
             break;
-          case 'password[i]':
-            this.value_vars += `<strong>${key}</strong> = ${this.password[this.i]}<br/>`
-            break;
-          case 'password_user[i]':
-            this.value_vars += `<strong>${key}</strong> = ${this.password_user[this.i]}<br/>`
+          case 'binary[i]':
+            var value = this.number % 2 === 0 ? '0' : '1';
+            this.value_vars += `<strong>${key}</strong> = ${value}<br/>`
             break;
           case 'i':
             this.value_vars += `<strong>${key}</strong> = ${this.i}<br/>`
             break;
-          case 'equal':
-            this.value_vars += `<strong>${key}</strong> = ${this.equal}<br/>`
+          case 'binary[index]':
+            var value = this.binary[this.index];
+            this.value_vars += `<strong>${key}</strong> = ${value}<br/>`
+            break;
+          case 'index':
+            this.value_vars += `<strong>${key}</strong> = ${this.index}<br/>`
             break;
           default:
             this.value_vars += `<strong>${key}</strong> = ${data[key as keyof typeof data]}<br/>`
@@ -269,6 +308,7 @@ export class Ejemplo1Component implements OnInit {
     this.validate_input();
     this.add_explain();
     this.add_top();
+    this.modify_vars();
     this.add_var();
     this.add_output();
     if ( this.current_line >= this.max_line ){
