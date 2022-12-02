8799776e1a1147966420dbc288424f40b3d474a8
juanpabloglobalsys98 <juanpabloperezdevelopment@gmail.com>
2022-12-02 02:34:07 -0500

    Adding module 1 concepto funcion example 4

--- src/app/ejemplos-concepto-funcion/ejemplo4/ejemplo4.component.ts
+++ src/app/ejemplos-concepto-funcion/ejemplo4/ejemplo4.component.ts
@@ -10,9 +10,9 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class Ejemplo4Component implements OnInit {
 
-  lines_to_input = [6, 8]
+  lines_to_input = [13, 15]
   current_line = 1;
-  max_line = 31;
+  max_line = 17;
   run_code = false;
   explain_pass = '';
   value_vars = '';
@@ -26,9 +26,108 @@ export class Ejemplo4Component implements OnInit {
   reload = false;
   inputfield = '';
 
-  code = ``;
+  // Variables del ejemplo
+  x:number=0;
+  n:number=0;
+  init_value :number=0;
 
-  code_obj = [];
+  code = `
+  #include <cstdio>
+  void serie(int x, int n){
+      int init_value = x;
+      printf("1 ");
+      while (init_value <= n){
+          printf("%d ",init_value);
+          init_value += x;
+      }
+  }
+  int main(){
+      int x,n;
+      printf("Ingrese el valor de la razon de cambio: ");
+      scanf("%d",&x);
+      printf("Ingrese el valor maximo de la serie: ");
+      scanf("%d",&n);
+      serie(x,n);
+  }`;
+
+  code_obj = [
+    {
+      'line_explain':'Se incluye la libreria <cstdio> la cual permite manejar funciones de entrada y salida',
+    },//1
+    {
+      'line_explain':'Se define la función serie, la cual recibe por parametro dos valores x, n',
+      'var_values':{
+        'x ':'',
+        'n ':'',
+      },
+    },//2
+    {
+      'line_explain':'Se declara una variable llamada init_value la cual va a ser igual al valor de x',
+      'var_values':{
+        'x ':'',
+        'init_value':'',
+      },
+    },//3
+    {
+      'line_explain':'Se imprime el número 1 como inicializador de la serie',
+    },//4
+    {
+      'line_explain':'Se hace un ciclo while el cual valida que el valor de la variable init_value sea menor o igual a n esto para asegurar que no se ha llegado al maximo de la serie',
+      'var_values':{
+        'n ':'',
+        'init_value':'',
+      },
+    },//5
+    {
+      'line_explain':'Se imprime el valor que hay en la variable init_value',
+      'var_values':{
+        'init_value':'',
+      },
+    },//6
+    {
+      'line_explain':'Luego, el valor de init_value va aumentar a la razón de cambio x',
+      'var_values':{
+        'x':'',
+        'init_value':'',
+      },
+    },//7
+    {
+      'line_explain':'Se cierra el ciclo while',
+    },//8
+    {
+      'line_explain':'Se cierra la función',
+    },//9
+    {
+      'line_explain':'Se declara la función principal',
+    },//10
+    {
+      'line_explain':'Se declara x,n',
+    },//11
+    {
+      'line_explain':'Se le pide al usuario que ingrese el valor de la razón de cambio',
+      'output':'Ingrese el valor de la razon de cambio:'
+    },//12
+    {
+      'line_explain':'Se almacena en la variable a el valor ingresado',
+    },//13
+    {
+      'line_explain':'Se le pide al usuario ingresar el valor maximo de la serie',
+      'output':'Ingrese el valor maximo de la serie:'
+    },//14
+    {
+      'line_explain':'Se almacena en la variable a el valor ingresado',
+    },//15
+    {
+      'line_explain':'Se hace el llamado a la función serie la cual imprime los numeros',
+      'var_values':{
+        'x':'',
+        'n':'',
+      },
+    },//16
+    {
+      'line_explain':'Fin del programa',
+    },//17
+  ];
 
   constructor(
     private highlightService: HighlightService,
@@ -69,26 +168,34 @@ export class Ejemplo4Component implements OnInit {
 
   jump = () => {
     switch (this.current_line) {
+      case 2:{
+        this.loop_jump(10,8);
+        break;
+      }
+      case 17:{
+        this.loop_jump(2,15,2);
+        break;
+      }
     }
   }
 
   validate_input = () => {
-    if (this.current_line === 7) {
+    if (this.current_line === 14) {
       if (this.inputfield === '' || isNaN(parseInt(this.inputfield))) {
         this.toastr.error('Debe ingresar un valor para continuar');
         this.current_line--;
         this.less_top();
       }
-      // this.a = parseInt(this.inputfield)
+      this.x = parseInt(this.inputfield)
       this.inputfield = '';
     }
-    if (this.current_line === 9) {
+    if (this.current_line === 16) {
       if (this.inputfield === '' || isNaN(parseInt(this.inputfield))) {
         this.toastr.error('Debe ingresar un valor para continuar');
         this.current_line--;
         this.less_top();
       }
-      // this.b = parseInt(this.inputfield)
+      this.n = parseInt(this.inputfield)
       this.inputfield = '';
     }
   }
@@ -112,6 +219,12 @@ export class Ejemplo4Component implements OnInit {
       let data = this.code_obj[this.current_line - 1]['var_values'];
       for (const key in data) {
         switch (key) {
+          case 'x':
+            this.value_vars += `<strong>${key}</strong> = ${this.x}<br/>`
+            break;
+          case 'n':
+            this.value_vars += `<strong>${key}</strong> = ${this.n}<br/>`
+            break;
           default:
             this.value_vars += `<strong>${key}</strong> = ${data[key as keyof typeof data]}<br/>`
             break
